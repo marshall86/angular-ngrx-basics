ad42f45064db252908e3f4637cc04969959c9444
marshall86 <antonio11061986@gmail.com>
2023-02-22 10:31:32 +0100

    counter.reducer --- decrement update

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -6,6 +6,8 @@ export const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1),
+  on(decrement, (state) => {
+    return state - 1 > 0 ? state - 1 : 0;
+  }),
   on(reset, (state) => 0)
 );
